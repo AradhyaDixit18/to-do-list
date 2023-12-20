26339973a1a1cf0cbbfd8ccd62d0e316d05556b5
Aradhya DIxit <aradhyadixit1974@gmail.com>
2023-12-20 19:40:06 +0530

    name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { TodoWrapper } from './components/TodoWrapper';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 function App() {
   return (
+   
     <div className="App">
       <TodoWrapper  />
-    </div>
-  );
-}
+      <div className="footer" >Made with &nbsp; <i className="fa fa-heart  heart" ></i>  &nbsp; Aradhya Dixit</div>
+  
+       </div>  
+)}
 
 export default App;
